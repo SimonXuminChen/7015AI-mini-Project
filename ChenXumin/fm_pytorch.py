ea2b2cbbf8b7a3c37c69597c4091a78206018ea6
Simon <simon_1101@163.com>
2019-11-29 14:15:07 +0800

    merge master

---
+++ ChenXumin/fm_pytorch.py
@@ -0,0 +1,11 @@
+#  -*- coding: utf-8 -*-
+'''
+===================================================
+@Time    : 2019/11/29  2:11 下午
+@Author  : Simon Chen Xumin
+@IDE     : PyCharm
+===================================================
+'''
+import pytorch
+mport torch.nn as nn
+class FM_Model(nn.model):
