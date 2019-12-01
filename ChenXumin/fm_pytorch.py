65addd9b5d35433eaa2b49aef9c570a4f5c21db0
Simon <simon_1101@163.com>
2019-12-01 22:28:52 +0800

    generator fm model by pytorch

--- ChenXumin/fm_pytorch.py
+++ ChenXumin/fm_pytorch.py
@@ -6,6 +6,30 @@
 @IDE     : PyCharm
 ===================================================
 '''
-import pytorch
-mport torch.nn as nn
-class FM_Model(nn.model):
+import torch
+import torch.nn as nn
+
+class FM_Layer(nn.Module):
+    def __init__(self,n=10,k=5):
+        super(FM_Layer, self).__init__()
+        self.n = n
+        self.k=k
+        self.linear = nn.Linear(self.n,1)
+        self.v=nn.Parameter(torch.randn(self.n,self.k))
+
+    def fm_layer(self,x):
+        linear_part = self.linear(x)
+        interaction_1 = torch.mm(x,self.v)
+        interaction_1 = torch.pow(interaction_1,2)
+        interaction_2 = torch.mm(torch.pow(x,2),torch.pow(self.v,2))
+        output = linear_part + torch.sum(0.5*interaction_2-interaction_1)
+        return output
+
+    def forward(self, x):
+        return self.fm_layer(x)
+
+
+fm = FM_Layer
+x = torch.randn(1,10)
+output = fm(x)
+
