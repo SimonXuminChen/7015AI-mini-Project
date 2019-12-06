961a31ff3c6acc87ca9cb9a339691c3c9667583d
Simon <simon_1101@163.com>
2019-12-06 17:10:50 +0800

    update preprocessing tools

--- tools/preprocessing.py
+++ tools/preprocessing.py
@@ -8,10 +8,12 @@
 '''
 import logging
 import numpy as np
-
+import pandas as pd
 import torch
-from torch.utils.data import DataLoader,Dataset
-from torch.autograd import Variable
+from torch.utils.data import Dataset
+
+continous_features = 13
+
 
 def load_data(filename):
     data = open(filename)
@@ -24,10 +26,11 @@ def load_data(filename):
             continue
         for x in range(len(lines) - 2):
             feature_tmp.append(float(lines[x]))
-        label.append(float(lines[-2])*2)
+        label.append(float(lines[-2]) * 2)
         feature.append(feature_tmp)
     data.close()
     return feature, label
+
 
 def outlier_detect(list, min=0., max=5.):
     flag = True
@@ -40,8 +43,8 @@ def outlier_detect(list, min=0., max=5.):
             break
         elif (list[i] == "" or list[i] is None):
             # there is a null value,so we have to delete this line
-            logging.warning( "There is a null, so this might be processed")
-            flag=False
+            logging.warning("There is a null, so this might be processed")
+            flag = False
             break
         else:
             new_list.append(list[i])
@@ -49,7 +52,7 @@ def outlier_detect(list, min=0., max=5.):
     if (not list[2].isalpha()) and ((float(list[2]) < min) or (float(list[2]) > max)):
         # there is a outlier value,so we have to delete this line
         logging.warning("There is outlier, so this might be processed")
-        flag=False
+        flag = False
 
     if flag is True:
         new_list.append(list[2])
@@ -58,27 +61,47 @@ def outlier_detect(list, min=0., max=5.):
         return []
 
 
-class movie_Dataset(Dataset):
-    def __init__(self,filename):
-        dataset = np.loadtxt(filename,delimiter=',',skiprows=1,dtype=np.float32)
+class PreProcessData(Dataset):
+    def __init__(self, filepath, train):
         self.feature = []
         self.label = []
-        for line in dataset.readlines():
-            feature_tmp = []
-            lines = outlier_detect(line.strip().split(','))
-            if len(lines) == 0:
-                continue
-            for x in range(len(lines) - 2):
-                feature_tmp.append(float(lines[x]))
-            self.label.append(float(lines[-2]) * 2)
-            self.feature.append(feature_tmp)
-        self.feature = torch.from_numpy(self.feature)
-        self.label = torch.from_numpy(self.label)
+        self.train = train
 
-        self.len=dataset.shape[0]
+        if self.train:
+            data = pd.read_csv(filepath)
+            self.train_data = data.iloc[:, :-2].values
+            self.target = data.iloc[:, -2].values
+
+        else:
+            data = pd.read_csv(filepath)
+            self.test_data = data.iloc[:, :-2].values
 
     def __getitem__(self, index):
-        return self.feature[index],self.label[index]
+        if self.train:
+            data_index, target_index = self.train_data[index, :], self.target[index]
+
+            Xi_coutinous = np.zeros_like(data_index[:continous_features])
+            Xi_categorial = data_index[continous_features:]
+            Xi = torch.from_numpy(np.concatenate((Xi_coutinous, Xi_categorial)).astype(np.int32)).unsqueeze(-1)
+
+            Xv_categorial = np.ones_like(data_index[continous_features:])
+            Xv_coutinous = data_index[:continous_features]
+            Xv = torch.from_numpy(np.concatenate((Xv_coutinous, Xv_categorial)).astype(np.int32))
+            return Xi, Xv, target_index
+        else:
+            data_index = self.train_data[index, :]
+
+            Xi_coutinous = np.zeros_like(data_index[:continous_features])
+            Xi_categorial = data_index[continous_features:]
+            Xi = torch.from_numpy(np.concatenate((Xi_coutinous, Xi_categorial)).astype(np.int32)).unsqueeze(-1)
+
+            Xv_categorial = np.ones_like(data_index[continous_features:])
+            Xv_coutinous = data_index[:continous_features]
+            Xv = torch.from_numpy(np.concatenate((Xv_coutinous, Xv_categorial)).astype(np.int32))
+            return Xi, Xv
 
     def __len__(self):
-        return self.len
+        if self.train:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
